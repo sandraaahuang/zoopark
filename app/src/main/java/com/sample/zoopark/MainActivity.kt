8cdda459cab0adf66f22745b74a34bf095b5906e
sandra <sandraaahuang@gmail.com>
2021-12-21 17:46:35 +0800

    init appcenter

--- app/src/main/java/com/sample/zoopark/MainActivity.kt
+++ app/src/main/java/com/sample/zoopark/MainActivity.kt
@@ -6,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 
 class MainActivity : AppCompatActivity() {
+
+
 
     private var navController: NavController? = null
 
@@ -21,6 +26,11 @@ class MainActivity : AppCompatActivity() {
         navController?.let {
             NavigationUI.setupActionBarWithNavController(this, it)
         }
+
+        AppCenter.start(
+            application, "f3cad314-fbee-4e12-9fa0-9baf7dbeff00",
+            Analytics::class.java, Crashes::class.java
+        )
 
     }
 
